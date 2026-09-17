0305c353dd68562f5594e0fe81e3dba991147e52
themacchimart <themacchimart@gmail.com>
2026-09-17 15:41:00 +0530

    Add Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ======================================================
+// THE MACCHI MART - FIREBASE CONFIG
+// ======================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8kfHBr-Kwc-Jf2HHtyBCllsr4A8ev6f0",
+  authDomain: "the-macchi-mart-7be07.firebaseapp.com",
+  projectId: "the-macchi-mart-7be07",
+  storageBucket: "the-macchi-mart-7be07.firebasestorage.app",
+  messagingSenderId: "981386840283",
+  appId: "1:981386840283:web:967a71c447387f612564fe",
+  measurementId: "G-ZLWDLY3QZ7"
+};
